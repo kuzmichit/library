196772802db3f9456c39885a48a0c6cea332a182
kuzmichit <kuzmichit@gmail.com>
2025-04-26 10:53:47 +0200

    commit save point

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -1,21 +1,20 @@
 const dropdown = ($) => {
-  
-$.prototype.dropdown = function() {
-  
-  for (let i = 0; i < this.length; i++) {
-    const id = this[i].getAttribute('id');
-    
-    function debounce() {
-      let a;
-      clearTimeout(a);
-      a = setTimeout( () => $(`[data-toggle-id = "${id}"]`).fadeToggle(100), 500)
+  $.prototype.dropdown = function () {
+    for (let i = 0; i < this.length; i++) {
+      const id = this[i].getAttribute('id');
+      let time;
+
+      // Assegna il click handler con debounce
+      $(this[i]).on('click', () => {
+        let currentTime = Date.now()
+        if(time > currentTime - 300) return;
+        $(`[data-toggle-id="${id}"]`).fadeToggle(100);
+        time = currentTime;
+      });
     }
-    $(this[i]).click(debounce); 
-  }
+  };
+
+  // $('.dropdown-toggle').dropdown();
 };
-
-
-$('dropdown-toggle').dropdown()
-}
 
 export default dropdown;
--- src/js/main.js
+++ src/js/main.js
@@ -12,9 +12,20 @@ const handler = (e) => {
   //iterate(attrs)
   console.log(iterate(attrs[1]))
 }
+$('.wrapper').html(`<div class="dropdown">
+          <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+          <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+            <a href="#" class="dropdown-item">Action</a>
+            <a href="#" class="dropdown-item">Action #2</a>
+            <a href="#" class="dropdown-item">Action #3</a>
+          </div>
+        </div>`);
+        
+$('.dropdown-toggle').dropdown();
 let s = () => console.log(333);
+// $('.dropdown-toggle').dropdown()
 
-$('button').click(()=> $('button').dropdown())
+
 /*const touchEnd = () => {
 let id = setTimeout(() => $('button').dropdown(), 1000);
 }*/
